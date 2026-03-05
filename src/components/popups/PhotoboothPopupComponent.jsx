56e86b48c956ae8346e9392346ef1c2a409d2dd2
Aakriti Rai <aakritip512@gmail.com>
2026-03-06 00:06:07 +0545

    photobooth popup completed

--- src/components/popups/PhotoboothPopupComponent.jsx
+++ src/components/popups/PhotoboothPopupComponent.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { RxCross2 } from "react-icons/rx";
+import img from "../../assets/images/ronnitxme.jpg"
 
 const PhotoboothPopupComponent = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-      <div className="bg-white p-6 rounded-lg relative w-80">
+      <div className="bg-white p-6 rounded-lg relative w-100">
         <button onClick={onClose} className="absolute top-2 right-2 text-xl">
           <RxCross2/>
         </button>
 
-        <h2 className="text-lg font-bold">Photobooth Popup</h2>
+        <h2 className="text-lg font-semibold text-center text-[#000035] my-2.5">Photobooth</h2>
+        <img src={img} alt="photobooth" className="rounded" />
+        <p className="mt-2 text-[#000035]">*Lets recreate this soon heheh</p>
+        <p className="text-red-600">ps- I was excited to show you this T-T <br /> hope you like it</p>
       </div>
     </div>
   );
