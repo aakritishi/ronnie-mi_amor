27daf311354c62d24538a84ea383d78dc73dc988
Aakriti Rai <aakritip512@gmail.com>
2026-03-07 01:08:35 +0545

    fix: minor changes on 2 components

--- src/components/popups/LetterPopupComponent.jsx
+++ src/components/popups/LetterPopupComponent.jsx
@@ -5,35 +5,39 @@ import letter from "../../data/letter.json";
 const LetterPopupComponent = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-      <div className="bg-white p-6 rounded-lg relative w-80">
+      <div className="bg-white p-6 rounded-lg relative w-80 h-[80vh]">
         <button onClick={onClose} className="absolute top-2 right-2 text-xl">
           <RxCross2 />
         </button>
 
-        {letter.map((data) => {
-          return (
-            <div key={data.id} className="text-[#000035]">
-              <h2 className="text-lg font-semibold">{data["first_segment"]}</h2>
-              <p>{data.second_segment}</p>
-              <p>{data.third_segment}</p>
-              <p>{data.third_segment}</p>
-              <p>{data.fourth_segment}</p>
-              <p>{data.fifth_segment}</p>
-              <p>{data.sixth_segment}</p>
-              <p>{data.seventh_segment}</p>
-              <p>{data.eighth_segment}</p>
-              <p>{data.ninth_segment}</p>
-              <p>{data.tenth_segment}</p>
-              <p>{data.eleventh_segment}</p>
-              <p>{data.twelfth_segment}</p>
-              <p>{data.thirteenth_segment}</p>
-              <p>{data.fourteenth_segment}</p>
-              <p>{data.fifteenth_segment}</p>
-              <p>{data.sixteenth_segment}</p>
-              <p>{data.seventeenth_segmement}</p>
-            </div>
-          );
-        })}
+        <div className="overflow-y-auto h-full">
+          {letter.map((data) => {
+            return (
+              <div key={data.id} className="text-[#000035]">
+                <h2 className="text-lg font-semibold">
+                  {data["first_segment"]}
+                </h2>
+                <p>{data.second_segment}</p>
+                <p>{data.third_segment}</p>
+                <p>{data.third_segment}</p>
+                <p>{data.fourth_segment}</p>
+                <p>{data.fifth_segment}</p>
+                <p>{data.sixth_segment}</p>
+                <p>{data.seventh_segment}</p>
+                <p>{data.eighth_segment}</p>
+                <p>{data.ninth_segment}</p>
+                <p>{data.tenth_segment}</p>
+                <p>{data.eleventh_segment}</p>
+                <p>{data.twelfth_segment}</p>
+                <p>{data.thirteenth_segment}</p>
+                <p>{data.fourteenth_segment}</p>
+                <p>{data.fifteenth_segment}</p>
+                <p>{data.sixteenth_segment}</p>
+                <p>{data.seventeenth_segmement}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/popups/PhotoboothPopupComponent.jsx
+++ src/components/popups/PhotoboothPopupComponent.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { RxCross2 } from "react-icons/rx";
-import img from "../../assets/images/ronnitxme.jpg"
+import img from "../../assets/images/ronnitxme.jpg";
 
 const PhotoboothPopupComponent = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-      <div className="bg-white p-6 rounded-lg relative w-100">
+      <div className="bg-white p-6 rounded-lg relative w-100 h-[90vh]">
         <button onClick={onClose} className="absolute top-2 right-2 text-xl">
-          <RxCross2/>
+          <RxCross2 />
         </button>
 
-        <h2 className="text-lg font-semibold text-center text-[#000035] my-2.5">Photobooth</h2>
-        <img src={img} alt="photobooth" className="rounded" />
-        <p className="mt-2 text-[#000035]">*Lets recreate this soon heheh</p>
-        <p className="text-red-600">ps- I was excited to show you this T-T <br /> hope you like it</p>
+        <div className="overflow-y-auto h-full">
+          <h2 className="text-lg font-semibold text-center text-[#000035] my-2.5">
+            Photobooth
+          </h2>
+          <img src={img} alt="photobooth" className="rounded" />
+          <p className="mt-2 text-[#000035]">*Lets recreate this soon heheh</p>
+          <p className="text-red-600">
+            ps- I was excited to show you this T-T <br /> hope you like it
+          </p>
+        </div>
       </div>
     </div>
   );
